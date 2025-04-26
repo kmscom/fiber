9eafecb6ca895c7a701fc8fc7ac04ca66739303c
kmscom <kmscom@naver.com>
2025-04-26 01:00:17 -0700

    Update main.go

--- main.go
+++ main.go
@@ -21,10 +21,11 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
-		})
-	})
+  c.Links(
+    "https://github.com/kmscom/Browser-Cache-Folder-Changer", "Web Browser Cache Folder Changer",
+    "https://github.com/kmscom/Browser-Cache-Folder-Changer/releases", "Download",
+  )
+})
 
 	app.Listen(getPort())
 }
